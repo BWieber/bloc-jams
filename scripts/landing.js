89c269dc6554b0985d6a6b8ca247045c4525f252
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-03-06 16:38:48 -0500

    Assignment 23: Callbacks complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
  
 var animatePoints = function(points) {  
-     var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
-    
-     for(var i=0; i < points.length; i ++) {
-         revealPoint(i);
-     }
+    forEach(points, revealPoint);
 };
 
+var revealPoint = function(points) {
+    points.style.opacity = 1;
+    points.style.transform = "scaleX(1) translateY(0)";
+    points.style.msTransform = "scaleX(1) translateY(0)";
+    points.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
  
  window.onload = function() {
-     
      if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
@@ -30,3 +26,4 @@ var animatePoints = function(points) {
          }
      });
  }
+ 
